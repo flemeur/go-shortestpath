26a970c171d652a1b3587eaf2d1719bb04428f97
Flemming Andersen <flemming@flamingcode.com>
2014-11-02 02:04:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+go-shortestpath
+===============
+
+A couple of implementations of different shortest path algorithms.
+Mostly just a learning project for me but might be useful for others as well.
+
+Please contribute if you find errors or have ideas for a better way of doing things.
+
+So far I've only implemented Dijkstra's algorithm but I plan to also do Bellman-Ford and maybe A\*.
+
+Dijkstra's shortest path algorithm
+----------------------------------
+
+	$ go get github.com/flemeur/go-shortestpath/dijkstra
+
+
---
+++ dijkstra/dijkstra.go
@@ -0,0 +1,111 @@
+// Copyright (c) 2014 Flemming Andersen.
+// Distributed under the MIT License (MIT)
+// which can be found in the LICENSE file.
+
+package dijkstra
+
+import (
+	"container/heap"
+	"errors"
+)
+
+// Node is an interface for your own implementation of a vertex in a graph
+type Node interface {
+	Edges() []Edge
+}
+
+// Edge is an interface for your own implementation of an edge between two vertices in a graph
+type Edge interface {
+	Destination() Node
+	Weight() float64
+}
+
+// ShortestPath finds a shortest path between the start and end nodes.
+// The two nodes' underlying values must be pointers
+func ShortestPath(start, end Node) ([]Node, error) {
+	visited := make(map[Node]bool)
+	dists := make(map[Node]float64)
+	prev := make(map[Node]Node)
+
+	dists[start] = 0
+	queue := &queue{&queueItem{value: start, weight: 0, index: 0}}
+	heap.Init(queue)
+
+	for queue.Len() > 0 {
+		// Done
+		if visited[end] {
+			break
+		}
+
+		item := heap.Pop(queue).(*queueItem)
+		n := item.value
+		for _, edge := range n.Edges() {
+			dest := edge.Destination()
+			dist := dists[n] + edge.Weight()
+			if tentativeDist, ok := dists[dest]; !ok || dist < tentativeDist {
+				dists[dest] = dist
+				prev[dest] = n
+				heap.Push(queue, &queueItem{value: dest, weight: dist})
+			}
+		}
+		visited[n] = true
+	}
+
+	if !visited[end] {
+		return nil, errors.New("no shortest path exists")
+	}
+
+	path := []Node{end}
+	for next := prev[end]; next != nil; next = prev[next] {
+		path = append(path, next)
+	}
+
+	// Reverse path
+	for i, j := 0, len(path)-1; i < j; i, j = i+1, j-1 {
+		path[i], path[j] = path[j], path[i]
+	}
+	return path, nil
+}
+
+type queueItem struct {
+	value  Node
+	weight float64
+	index  int
+}
+
+type queue []*queueItem
+
+func (q queue) Len() int {
+	return len(q)
+}
+
+func (q queue) Less(i, j int) bool {
+	return q[i].weight < q[j].weight
+}
+
+func (q queue) Swap(i, j int) {
+	q[i], q[j] = q[j], q[i]
+	q[i].index = i
+	q[j].index = j
+}
+
+func (q *queue) Push(x interface{}) {
+	n := len(*q)
+	item := x.(*queueItem)
+	item.index = n
+	*q = append(*q, item)
+}
+
+func (q *queue) Pop() interface{} {
+	old := *q
+	n := len(old)
+	item := old[n-1]
+	item.index = -1 // for safety
+	*q = old[0 : n-1]
+	return item
+}
+
+func (q *queue) update(item *queueItem, weight float64) {
+	item.weight = weight
+	heap.Fix(q, item.index)
+}
